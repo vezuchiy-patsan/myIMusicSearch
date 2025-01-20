3b2b054ba46711f8ce3df3e7f2ba7d802d5bd44d
Daniil <daniil.axiyan.316@mail.ru>
2025-01-20 14:35:51 +0300

    gh-page

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-	base: '/myIMusicSearch/',
+	base: '/myIMusicSearch',
 	plugins: [react()],
 	resolve: {
 		alias: [
