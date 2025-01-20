688c24015fcf15eea0f145455524801e4f8b4787
Daniil <daniil.axiyan.316@mail.ru>
2025-01-20 14:22:49 +0300

    rename dir

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,16 @@ import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+	base: '/myIMusicSearch/',
 	plugins: [react()],
 	resolve: {
 		alias: [
 			{ find: '@', replacement: path.resolve(__dirname, 'src') },
 			// https://github.com/tabler/tabler-icons/issues/1233#issuecomment-2428245119 проблемы с динамическим импортом пак иконок
-			{ find: '@tabler/icons-react', replacement: '@tabler/icons-react/dist/esm/icons/index.mjs' },
+			{
+				find: '@tabler/icons-react',
+				replacement: '@tabler/icons-react/dist/esm/icons/index.mjs',
+			},
 		],
 	},
 	css: {
